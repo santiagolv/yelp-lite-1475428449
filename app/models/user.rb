2f90d3d847c0895e343223f5efac74553a47c233
Raghu Betina <raghu@firstdraft.com>
2016-10-02 17:17:00 +0000

    Direct association between users and review_replies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :review_replies,
+             :dependent => :destroy
+
   has_many   :restaurant_ownerships,
              :dependent => :destroy
 
